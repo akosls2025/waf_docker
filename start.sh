0298cfa4a8b65dbc1823cf7e74bc8ee8eaf4e91d
akosls2025 <akos.gyori.ls2025@gmail.com>
2025-04-25 22:14:23 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8443
+if command -v docker-compose &> /dev/null; then
+docker-compose up
+elif docker compose version &> /dev/null; then
 docker compose up
+else
+echo "Neither docker-compose nor docker compose is available."
+exit 1
+fi
 
 
